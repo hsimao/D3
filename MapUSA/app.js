68edd6858b3bdfafd6741782b89bb0b5c19b3bae
hsimao <e087754958@gmail.com>
2019-10-24 20:15:28 +0800

    edit: 將功能拆分, 用 function 拼裝

--- MapUSA/app.js
+++ MapUSA/app.js
@@ -24,50 +24,72 @@ function makeFillColor(val, colors) {
   return num ? colors(num) : '#ddd'
 }
 
-// 創建投影, 將 3D 地圖轉換成 2D
-const projection = d3
-  .geoAlbersUsa()
-  .scale([chartWidth])
-  .translate([chartWidth / 2, chartHeight / 2])
+let projection, path, svg
 
-const path = d3.geoPath(projection)
+function mapInit() {
+  // 創建投影, 將 3D 地圖轉換成 2D
+  projection = d3
+    .geoAlbersUsa()
+    .scale([chartWidth])
+    .translate([chartWidth / 2, chartHeight / 2])
 
-// Create SVG
-var svg = d3
-  .select('#chart')
-  .append('svg')
-  .attr('width', chartWidth)
-  .attr('height', chartHeight)
+  path = d3.geoPath(projection)
 
-// data
-d3.json('zombie-attacks.json').then(zombieData => {
-  // 依據 zombie 資料的 num 數據來產生顏色
-  mapColor.domain([d3.min(zombieData, d => d.num), d3.max(zombieData, d => d.num)])
+  // Create SVG
+  svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', chartWidth)
+    .attr('height', chartHeight)
 
-  d3.json('us.json').then(usData => {
-    usData.features.forEach((usE, usI) => {
-      zombieData.forEach((zE, zI) => {
-        // 如果 zombieData 資料跟 us 地圖資料名稱有吻合才將 num 資料寫入地圖
-        if (usE.properties.name !== zE.state) return null
-        usData.features[usI].properties.num = parseFloat(zE.num)
+  drawMap()
+  handleDirectionButton()
+}
+mapInit()
+
+// 產生繪製地圖的資料
+function makeMapData() {
+  return new Promise((resolve, reject) => {
+    d3.json('zombie-attacks.json')
+      .then(zombieData => {
+        // 依據 zombie 資料的 num 數據來產生顏色
+        mapColor.domain([d3.min(zombieData, d => d.num), d3.max(zombieData, d => d.num)])
+
+        d3.json('us.json')
+          .then(usData => {
+            usData.features.forEach((usE, usI) => {
+              zombieData.forEach((zE, zI) => {
+                // 如果 zombieData 資料跟 us 地圖資料名稱有吻合才將 num 資料寫入地圖
+                if (usE.properties.name !== zE.state) return null
+                usData.features[usI].properties.num = parseFloat(zE.num)
+              })
+            })
+
+            resolve(usData.features)
+          })
+          .catch(err => reject(err))
       })
-    })
-
-    // console.log(usData)
-
-    svg
-      .selectAll('path')
-      .data(usData.features)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      .attr('fill', d => makeFillColor(d, mapColor))
-      .attr('stroke', strokeColor)
-      .attr('stroke-width', strokeWidth)
-
-    drawCities()
+      .catch(err => reject(err))
   })
-})
+}
+
+// 繪製地圖
+async function drawMap() {
+  const data = await makeMapData()
+  if (!data) return
+
+  svg
+    .selectAll('path')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('fill', d => makeFillColor(d, mapColor))
+    .attr('stroke', strokeColor)
+    .attr('stroke-width', strokeWidth)
+
+  drawCities()
+}
 
 // 繪製 city
 function drawCities() {
@@ -100,38 +122,40 @@ function drawCities() {
 }
 
 // 控制方向按鈕
-d3.selectAll('#buttons button').on('click', function() {
-  const offset = projection.translate()
+function handleDirectionButton() {
+  d3.selectAll('#buttons button').on('click', function() {
+    const offset = projection.translate()
 
-  const distance = 100
+    const distance = 100
 
-  // 取得點擊當下按鈕得 class 來判斷方向
-  const direction = d3.select(this).attr('class')
-  if (direction === 'up') offset[1] -= distance
-  if (direction === 'down') offset[1] += distance
-  if (direction === 'left') offset[0] -= distance
-  if (direction === 'right') offset[0] += distance
+    // 取得點擊當下按鈕得 class 來判斷方向
+    const direction = d3.select(this).attr('class')
+    if (direction === 'up') offset[1] -= distance
+    if (direction === 'down') offset[1] += distance
+    if (direction === 'left') offset[0] -= distance
+    if (direction === 'right') offset[0] += distance
 
-  // 更新投影位置
-  projection.translate(offset)
+    // 更新投影位置
+    projection.translate(offset)
 
-  // 更新地圖
-  svg
-    .selectAll('path')
-    .transition() // 加上 transition 增加平滑移動效果
-    .attr('d', path)
+    // 更新地圖
+    svg
+      .selectAll('path')
+      .transition() // 加上 transition 增加平滑移動效果
+      .attr('d', path)
 
-  // 更新 city 圓圈
-  svg
-    .selectAll('circle')
-    .transition()
-    .attr('cx', d => projection([d.lon, d.lat])[0])
-    .attr('cy', d => projection([d.lon, d.lat])[1])
+    // 更新 city 圓圈
+    svg
+      .selectAll('circle')
+      .transition()
+      .attr('cx', d => projection([d.lon, d.lat])[0])
+      .attr('cy', d => projection([d.lon, d.lat])[1])
 
-  // 更新 city 文字
-  svg
-    .selectAll('text')
-    .transition()
-    .attr('x', d => projection([d.lon, d.lat])[0] - 18)
-    .attr('y', d => projection([d.lon, d.lat])[1] + 3)
-})
+    // 更新 city 文字
+    svg
+      .selectAll('text')
+      .transition()
+      .attr('x', d => projection([d.lon, d.lat])[0] - 18)
+      .attr('y', d => projection([d.lon, d.lat])[1] + 3)
+  })
+}
